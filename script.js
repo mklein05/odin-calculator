a34e45b0dc4a56c504ada99ceb2b0ab6364a9aeb
mklein05 <milans.kleins05@gmail.com>
2025-06-20 10:52:37 +0100

    Added more features to calculator and improved styling.

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let num1;
 let num2;
 let operator = null;
 let chainingOperators = false;
+let justEquated;
 
 const display = document.querySelector(".display");
+display.textContent = "";
 
 const numbers = document.querySelectorAll(".number");
 
@@ -12,6 +14,10 @@ const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
 
 const clear = document.querySelector(".clear");
+
+const decimalPoint = document.querySelector(".decimal");
+
+const backSpace = document.querySelector(".backspace");
 
 for(let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener("click", enterNum);
@@ -23,7 +29,11 @@ for(let i = 0; i < operators.length; i++){
 
 equals.addEventListener("click", equate);
 
-clear.addEventListener("click", clearCalc );
+clear.addEventListener("click", clearCalc);
+
+decimalPoint.addEventListener("click", addDecimal);
+
+backSpace.addEventListener("click", useBackSpace);
 
 function enterNum(event){
     if(chainingOperators){
@@ -31,36 +41,61 @@ function enterNum(event){
         chainingOperators = false;
         display.textContent += event.target.textContent;
     }
+    else if (justEquated){
+        display.textContent = event.target.textContent;
+        justEquated = false;
+    }
     else{
         display.textContent += event.target.textContent;
     }
 
 }
 
+function addDecimal(){
+    if(display.textContent != "" && display.textContent.includes(".") == false){
+        display.textContent += ".";
+    }
+}
+
+
 function setOperator(event){
     if(display.textContent == ""){
         operator = event.target.textContent;
         return;
     }
+    if(chainingOperators){       
+        display.textContent = "";
+        chainingOperators = false;
+    }
     if(operator == null){
         num1 = parseFloat(display.textContent);
         operator = event.target.textContent;
-        display.textContent = ""
+        display.textContent = "";
     }
     else{
         chainingOperators = true; 
         equate();
-        num1 = parseFloat(display.textContent);
+        justEquated = false;
+        if(display.textContent != ""){
+            num1 = parseFloat(display.textContent);
+        }
+        else{
+            num1 = parseFloat(ans);
+        }
         operator = event.target.textContent;
     }
 }
 
 
 function equate(){
-    num2 = parseInt(display.textContent);
+    if(display.textContent == ""){
+        return;
+    }
+    num2 = parseFloat(display.textContent);
     ans = operate(operator, num1, num2);
     operator = null
     display.textContent = parseFloat(ans.toFixed(3));
+    justEquated = true;
 }
 
 function clearCalc(){
@@ -69,6 +104,12 @@ function clearCalc(){
     num2 = null;
     ans = null;
     operator = null;
+}
+
+function useBackSpace (){
+    if(display.textContent != ""){
+        display.textContent = display.textContent.slice(0, -1);
+    }
 }
 
 function add(num1, num2){
@@ -84,6 +125,10 @@ function multiply(num1,num2){
 }
 
 function divide(num1,num2){
+    if(num2 == 0){
+        alert("You cant divide by zero!");
+        return num1 / 1;
+    }
     return num1 / num2;
 }
 
